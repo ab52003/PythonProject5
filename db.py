e9e5b6307dd33b270ce616b8ad04d890978e599f
ab52003 <ab52003@mail.ru>
2025-01-27 09:49:45 +0300

    Add files via upload

--- db.py
+++ db.py
@@ -16,10 +16,22 @@ cursor.execute("CREATE INDEX IF NOT EXISTS idx_age ON Users (age)")
 #for i in range(1, 11, 3):
     #cursor.execute("DELETE FROM Users WHERE username = ?", (f"User{i}",))
 
-cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", (60,))
-users = cursor.fetchall()
-for user in users:
-    print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}')
+#cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", (60,))
+#users = cursor.fetchall()
+#for user in users:
+    #print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}')
+
+cursor.execute("DELETE FROM Users WHERE id = ?", ("6"))
+cursor.execute("SELECT COUNT(*) FROM Users")
+total_users = cursor.fetchone()[0]
+cursor.execute("SELECT SUM(balance) FROM Users")
+all_balances = cursor.fetchone()[0]
+cursor.execute("SELECT AVG(balance) FROM Users")
+avg_balances = cursor.fetchone()[0]
+print(avg_balances)
+print(all_balances/total_users)
+
+#cursor.execute("DELETE FROM Users")
 
 connection.commit()
 connection.close()
